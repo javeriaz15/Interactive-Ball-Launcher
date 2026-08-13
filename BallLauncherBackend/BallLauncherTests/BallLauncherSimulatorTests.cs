f1676ccc9f350edc842a5feecba0904f0f0dec4b
Juwairiah Zia <javeriazia15@gmail.com>
2026-08-13 14:15:58 -0500

    test: strengthen simulator unit test coverage

--- BallLauncherBackend/BallLauncherTests/BallLauncherSimulatorTests.cs
+++ BallLauncherBackend/BallLauncherTests/BallLauncherSimulatorTests.cs
@@ -4,19 +4,49 @@ using BallLauncherApi;
 public class BallLauncherSimulatorTests
 {
     [Fact]
-    public void CalculateLaunchParameters_ValidInputs_ReturnsCorrectValues()
+    public void CalculateLaunchParameters_ValidInputs_ReturnsExpectedValues()
     {
-        var simulator = new BallLauncherSimulator(0, 2, 45, 1);
+        // Arrange
+        var simulator = new BallLauncherSimulator(
+            startingAngle: 0,
+            motorTorque: 2,
+            ballReleaseAngle: 45,
+            ballWeight: 1
+        );
+
+        // Act
         var result = simulator.CalculateLaunchParameters();
-        Assert.True(result.distance > 0, "Distance should be greater than 0");
-        Assert.True(result.maxHeight > 0, "Max Height should be greater than 0");
+
+        // Assert
+        Assert.Equal(40.816327, result.distance, 6);
+        Assert.Equal(10.204082, result.maxHeight, 6);
     }
 
-    [Fact]
-    public void CalculateLaunchParameters_InvalidBallWeight_ReturnsZero()
+    [Theory]
+    [InlineData(0, 2, 45, 0)]    // Zero ball weight
+    [InlineData(0, 2, 45, -1)]   // Negative ball weight
+    [InlineData(0, 0, 45, 1)]    // Zero motor torque
+    [InlineData(0, -1, 45, 1)]   // Negative motor torque
+    [InlineData(0, 2, 0, 1)]     // Release angle at lower boundary
+    [InlineData(0, 2, 90, 1)]    // Release angle at upper boundary
+    public void CalculateLaunchParameters_InvalidInputs_ReturnsZero(
+        double startingAngle,
+        double motorTorque,
+        double ballReleaseAngle,
+        double ballWeight)
     {
-        var simulator = new BallLauncherSimulator(0, 2, 45, 0);
+        // Arrange
+        var simulator = new BallLauncherSimulator(
+            startingAngle,
+            motorTorque,
+            ballReleaseAngle,
+            ballWeight
+        );
+
+        // Act
         var result = simulator.CalculateLaunchParameters();
+
+        // Assert
         Assert.Equal(0, result.distance);
         Assert.Equal(0, result.maxHeight);
     }
