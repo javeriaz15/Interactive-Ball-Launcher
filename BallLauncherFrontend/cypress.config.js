bc19efbadb19356145d226c4625b5a175d1ef680
Juwairiah Zia <javeriazia15@gmail.com>
2026-08-13 13:40:40 -0500

    test: consolidate Cypress configuration and add test scripts

--- BallLauncherFrontend/cypress.config.js
+++ BallLauncherFrontend/cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
+    pageLoadTimeout: 120000,
+    defaultCommandTimeout: 10000,
+    slowTestThreshold: 5000,
   },
+  video: false,
 });
